ed83c95328ea3612f4b465fe452ad840c05b5d57
jhonvalen <jhonv@seas.upenn.edu>
2019-04-11 20:07:29 -0400

    Added another print line documenting the day of the cange

--- GitTest.java
+++ GitTest.java
@@ -7,6 +7,10 @@ public class GitTest {
 	
 	public static void main(String[] args) {
 		System.out.println("this is a test");
+		System.out.println("this is test number 2 \n added after the first commit");
+		System.out.println("this is test number 3 \n also added after the first commit");
+
+		System.out.println("Test commit on April 11, 2019. \n collaborating with Gina");		
 	}
 
 }
