ea1b7d81f0aad334393f441ead848ef72622fc73
jhonvalen <jhonv@seas.upenn.edu>
2019-04-11 20:29:57 -0400

    Trying to create a merge conflict

--- GitTest.java
+++ GitTest.java
@@ -11,7 +11,7 @@ public class GitTest {
 		System.out.println("this is test number 3 \n also added after the first commit");
 
 		System.out.println("Test commit on April 11, 2019. \n collaborating with Gina");	
-		System.out.println("Let's change here  \n collaborating with Gina");		
+		System.out.println("Let's change here Jhon edited this line at 8:25 PM  \n collaborating with Gina");		
 	}
 
 }
