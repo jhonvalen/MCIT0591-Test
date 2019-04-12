4f07ba73f5e074cadad4c9c5fe33780bbf500f19
Gina Jung <ginajung@seas.upenn.edu>
2019-04-12 09:21:51 +0900

    second commit

--- GitTest.java
+++ GitTest.java
@@ -10,7 +10,8 @@ public class GitTest {
 		System.out.println("this is test number 2 \n added after the first commit");
 		System.out.println("this is test number 3 \n also added after the first commit");
 
-		System.out.println("Test commit on April 11, 2019. \n collaborating with Gina");		
+		System.out.println("Test commit on April 11, 2019. \n collaborating with Gina");	
+		System.out.println("Let's change here  \n collaborating with Gina");		
 	}
 
 }
