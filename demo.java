5fcfd9924e266a418e165cfb1c19c4199dcb5caa
eqin <qcslam@gmail.com>
2019-04-14 20:58:24 +1000

    where I'm from

--- demo.java
+++ demo.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         
         System.out.println("Hello, I'm Eric");
+        System.out.println("I'm from Sydney, Australia");
     }
 
 }
